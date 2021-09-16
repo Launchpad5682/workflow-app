af025b30524c36be16da6525996e8d3357fb5199
launchpad5682 <launchpad5682@gmail.com>
2021-09-16 18:31:30 +0530

    add edge for an action

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import "./App.css";
-import ReactFlow from "react-flow-renderer";
+import "./styles/output.css";
+import ReactFlow, { addEdge } from "react-flow-renderer";
 import { useWorkflowContext } from "./context/WorkflowContext";
+import { useModalOverlayContext } from "./context/ModalOverlayContext";
+import SideModal from "./components/SideModal";
 
 function App() {
-  const { elements } = useWorkflowContext();
+  const { elements, setElements } = useWorkflowContext();
+  const { modalOverlay, setModalOverlay } = useModalOverlayContext();
+
+  const modalSlideInput = (event) => {
+    setModalOverlay(!modalOverlay);
+  };
+
+  const onConnect = (params) => setElements((els) => addEdge(params, els));
 
   return (
-    <div style={{ height: 500 }}>
-      <button>Add Action</button>
-      <ReactFlow elements={elements} />
+    <div className=" h-screen flex-col items-center justify-center">
+      <button
+        onClick={modalSlideInput}
+        className="bg-blue-500 w-32 h-12 rounded-lg text-white"
+      >
+        Add Action
+      </button>
+      {modalOverlay && <SideModal />}
+      {/* The dimensions of your React Flow component depend on the parents dimensions. */}
+      <div className="h-5/6 w-5/6  bg-white border-red-900 border-solid border-2">
+        <ReactFlow elements={elements} onConnect={onConnect} />
+      </div>
     </div>
   );
 }
