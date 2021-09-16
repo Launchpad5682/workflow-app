ac548c054454c00a645afaa576e6a69dbb849ad0
launchpad5682 <launchpad5682@gmail.com>
2021-09-16 16:29:31 +0530

    some tailwind update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ReactFlow from "react-flow-renderer";
+import { useWorkflowContext } from "./context/WorkflowContext";
 
 function App() {
+  const { elements } = useWorkflowContext();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: 500 }}>
+      <button>Add Action</button>
+      <ReactFlow elements={elements} />
     </div>
   );
 }
--- src/context/WorkflowContext.js
+++ src/context/WorkflowContext.js
@@ -1,11 +1,34 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const WorkflowContext = createContext();
 
 export const useWorkflowContext = () => useContext(WorkflowContext);
 
 export const WorkflowProvider = (props) => {
-  const value = {};
+  const [elements, setElements] = useState([
+    {
+      id: "1",
+      type: "input", // input node
+      data: { label: "Input Node" },
+      position: { x: 250, y: 25 },
+    },
+    // default node
+    {
+      id: "2",
+      data: { label: <div>Default Node</div> },
+      position: { x: 100, y: 125 },
+    },
+    {
+      id: "3",
+      type: "output",
+      data: { label: "Output Node" },
+      position: { x: 250, y: 250 },
+    },
+    { id: "e1-2", source: "1", target: "2", animated: true },
+    { id: "e2-3", source: "2", target: "3" },
+  ]);
+
+  const value = { elements, setElements };
   return (
     <WorkflowContext.Provider value={value}>
       {props.children}
