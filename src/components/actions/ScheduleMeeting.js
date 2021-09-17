1b04a9da2973a38eca4d4ff61e6bc531a54db837
launchpad5682 <launchpad5682@gmail.com>
2021-09-18 01:39:29 +0530

    Schedule Meeting linked with addition of action

--- src/components/actions/ScheduleMeeting.js
+++ src/components/actions/ScheduleMeeting.js
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { HiLocationMarker, HiMail } from "react-icons/hi";
 import { useModalOverlayContext } from "../../context/ModalOverlayContext";
+import { useWorkflowContext } from "../../context/WorkflowContext";
+import { idGenerator } from "../../helper/idGenerator";
 
 const ScheduleMeeting = () => {
   const { setScheduleMeetingOverlay } = useModalOverlayContext();
+  const { actions, setActions } = useWorkflowContext();
+  const [emails, setEmails] = useState([]);
+  const emailRef = useRef(null);
+  const locationRef = useRef(null);
+  const deadlineRef = useRef(null);
 
   const closeModal = (event) => {
     event.preventDefault();
     setScheduleMeetingOverlay(false);
   };
 
-  const addMeetingAction = () => {};
+  const addMeetingAction = (event) => {
+    event.preventDefault();
+    const id = idGenerator();
+    const emailList = emails;
+    const location = locationRef.current.value;
+    const deadline = deadlineRef.current.value;
+    locationRef.current.value = null;
+    deadlineRef.current.value = null;
+
+    const action = {
+      id: id,
+      label: "Schedule Meeting",
+      emailList: emailList,
+      location: location,
+      deadline: deadline,
+    };
+    
+    setActions([...actions, action]);
+    setScheduleMeetingOverlay(false);
+  };
+
+  const addMail = (event) => {
+    event.preventDefault();
+    console.log(emailRef.current.value);
+    setEmails([...emails, emailRef.current.value]);
+    emailRef.current.value = null;
+  };
 
   return (
-    <div>
+    <form onSubmit={addMeetingAction}>
       <div>
         <h1>Meeting</h1>
         <div className="h-24 w-full">
@@ -23,11 +56,16 @@ const ScheduleMeeting = () => {
               type="email"
               placeholder="Enter email to add for a meeting"
               className="outline-none w-full pl-4"
+              ref={emailRef}
             />
-            <button className="w-2/12 rounded-md bg-blue-500 text-white h-full">
+            <button
+              className="w-2/12 rounded-md bg-blue-500 text-white h-full"
+              onClick={addMail}
+            >
               Add
             </button>
           </div>
+          {emails ? emails.map((email) => <span>{email}</span>) : null}
         </div>
       </div>
       <div className="w-full">
@@ -38,19 +76,17 @@ const ScheduleMeeting = () => {
             type="url"
             placeholder="Add a location"
             className="outline-none w-full pl-4"
+            ref={locationRef}
           />
-          <button className="w-2/12 rounded-md bg-blue-500 text-white h-full">
-            Add
-          </button>
         </div>
       </div>
       <div>
         <label className="block">Deadline</label>
-        <input type="datetime-local" />
+        <input type="datetime-local" ref={deadlineRef} />
       </div>
       <div className="mt-9">
         <button
-          onClick={addMeetingAction}
+          type="submit"
           className="w-24 h-10 rounded-lg bg-green-600 text-white font-bold"
         >
           SUBMIT
@@ -62,7 +98,7 @@ const ScheduleMeeting = () => {
           CANCEL
         </button>
       </div>
-    </div>
+    </form>
   );
 };
 
