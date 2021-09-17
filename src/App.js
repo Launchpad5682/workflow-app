29ba7c4978533930c560069e30b8e6e49729fab1
launchpad5682 <launchpad5682@gmail.com>
2021-09-17 10:30:46 +0530

    Action buttons added

--- src/App.js
+++ src/App.js
@@ -3,31 +3,76 @@ import "./styles/output.css";
 import ReactFlow, { addEdge } from "react-flow-renderer";
 import { useWorkflowContext } from "./context/WorkflowContext";
 import { useModalOverlayContext } from "./context/ModalOverlayContext";
-import AddActionModal from "./components/AddActionModal";
+import SideModal from "./components/SideModal";
+import SendEmail from "./components/actions/SendEmail";
+import ScheduleMeeting from "./components/actions/ScheduleMeeting";
+import SetReminder from "./components/actions/SetReminder";
 
 function App() {
   const { elements, setElements } = useWorkflowContext();
-  const { modalOverlay, setModalOverlay } = useModalOverlayContext();
+  const {
+    sendEmailOverlay,
+    setSendEmailOverlay,
+    scheduleMeetingOverlay,
+    setScheduleMeetingOverlay,
+    reminderOverlay,
+    setReminderOverlay,
+  } = useModalOverlayContext();
 
   const modalSlideInput = (event) => {
-    setModalOverlay(!modalOverlay);
+    const action = event.target.id;
+    if (action === "sendEmail") setSendEmailOverlay(true);
+    else if (action === "scheduleMeeting") setScheduleMeetingOverlay(true);
+    else setReminderOverlay(true);
   };
 
   const onConnect = (params) => setElements((els) => addEdge(params, els));
   const onElementClick = (event, element) => {
     console.log("click", element);
-    setModalOverlay(true);
+    //setModalOverlay(true);
   };
 
   return (
     <div className=" h-screen flex-col items-center justify-center px-6 py-6">
-      <button
-        onClick={modalSlideInput}
-        className="bg-blue-500 w-32 h-12 rounded-lg text-white mb-3"
-      >
-        Add Action
-      </button>
-      {modalOverlay && <AddActionModal />}
+      <div className="pb-3">
+        <span className="text-2xl font-serif mr-3">Actions</span>
+        <button
+          onClick={modalSlideInput}
+          id="sendEmail"
+          className="bg-red-500 w-48 h-12 rounded-lg text-white mr-3 font-bold"
+        >
+          Send Email
+        </button>
+        <button
+          onClick={modalSlideInput}
+          id="scheduleMeeting"
+          className="bg-green-500 w-48 h-12 rounded-lg text-white mr-3 font-bold"
+        >
+          Schedule Meeting
+        </button>
+        <button
+          onClick={modalSlideInput}
+          id="setReminder"
+          className="bg-yellow-500 w-48 h-12 rounded-lg text-white mb-3 font-bold"
+        >
+          Set Reminder
+        </button>
+      </div>
+      {sendEmailOverlay ? (
+        <SideModal action="sendEmail">
+          <SendEmail />
+        </SideModal>
+      ) : null}
+      {scheduleMeetingOverlay ? (
+        <SideModal action="scheduleMeeting">
+          <ScheduleMeeting />
+        </SideModal>
+      ) : null}
+      {reminderOverlay ? (
+        <SideModal action="setReminder">
+          <SetReminder />
+        </SideModal>
+      ) : null}
       {/* The dimensions of your React Flow component depend on the parents dimensions. */}
       <div className="h-5/6 w-full  bg-white border-red-900 border-solid border-2">
         <ReactFlow
