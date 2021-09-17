37dd0047ad935acbd1dcd12d47b378e37f9a4dcb
launchpad5682 <launchpad5682@gmail.com>
2021-09-17 21:20:07 +0530

    actions will update elements to render actions in the flow diagram

--- src/context/WorkflowContext.js
+++ src/context/WorkflowContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const WorkflowContext = createContext();
 
@@ -24,12 +24,44 @@ export const WorkflowProvider = (props) => {
       data: { label: "Output Node" },
       position: { x: 250, y: 250 },
     },
+    {
+      id: "4ce3",
+      data: { label: "Send Email" },
+      position: { x: 300, y: 300 },
+    },
     { id: "e1-2", source: "1", target: "2", animated: true },
   ];
 
   const [elements, setElements] = useState(initialState);
+  const [actions, setActions] = useState([
+    { id: "4f82", label: "Send Mail" },
+    { id: "4f83", label: "Send Mail" },
+  ]);
 
-  const value = { elements, setElements };
+  useEffect(() => {
+    let tempElements = [];
+    actions.forEach((action) => {
+      const x = Math.floor(Math.random() * 400);
+      const y = Math.floor(Math.random() * 400);
+      const label = action.label;
+      if (label === "Send Mail") {
+        const id = action.id;
+        const ele = {
+          id: id,
+          type: "default",
+          data: { label: action.label },
+          position: { x: x, y: y },
+        };
+        tempElements.push(ele);
+      } else if (label === "Schedule Meeting") {
+      } else {
+      }
+    });
+
+    setElements(tempElements);
+  }, [actions]);
+
+  const value = { elements, actions, setElements, setActions };
   return (
     <WorkflowContext.Provider value={value}>
       {props.children}
