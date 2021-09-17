8e253c480961544229eea4019ce9c47f02136e27
launchpad5682 <launchpad5682@gmail.com>
2021-09-17 21:19:14 +0530

    Side modal updated

--- src/components/SideModal.js
+++ src/components/SideModal.js
@@ -13,10 +13,6 @@ const SideModal = ({ children, action }) => {
     else setReminderOverlay(false);
   };
 
-  const addAction = (event) => {
-    event.preventDefault();
-  };
-
   return (
     <div className="inset-0 fixed bg-gray-900 bg-opacity-40 z-10">
       <div className="h-full w-4/12 bg-white float-right px-10 flex-col font-serif pt-4">
@@ -28,20 +24,6 @@ const SideModal = ({ children, action }) => {
           />
         </div>
         {children ? children : null}
-        <div className="mt-9">
-          <button
-            onClick={addAction}
-            className="w-24 h-10 rounded-lg bg-green-600 text-white font-bold"
-          >
-            SUBMIT
-          </button>
-          <button
-            onClick={closeModal}
-            className="w-24 h-10 rounded-lg bg-red-600 text-white font-bold ml-5"
-          >
-            CANCEL
-          </button>
-        </div>
       </div>
     </div>
   );
