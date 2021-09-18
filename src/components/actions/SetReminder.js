08cef5019353a9c92f8fd8497ed24992419aec81
launchpad5682 <launchpad5682@gmail.com>
2021-09-18 15:17:28 +0530

    input form updated

--- src/components/actions/SetReminder.js
+++ src/components/actions/SetReminder.js
@@ -1,45 +1,83 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { HiClock } from "react-icons/hi";
 import { useModalOverlayContext } from "../../context/ModalOverlayContext";
+import { useWorkflowContext } from "../../context/WorkflowContext";
+import { idGenerator } from "../../helper/idGenerator";
 
 const SetReminder = () => {
   const { setReminderOverlay } = useModalOverlayContext();
+  const { actions, setActions } = useWorkflowContext();
+  const [emails, setEmails] = useState([]);
+  const emailRef = useRef(null);
+  const noteRef = useRef(null);
+  const deadlineRef = useRef(null);
 
   const closeModal = (event) => {
     event.preventDefault();
     setReminderOverlay(false);
   };
 
-  const addSetReminderAction = () => {};
+  const addSetReminderAction = (event) => {
+    event.preventDefault();
+    const id = idGenerator();
+    const emailList = emails;
+    const reminder = noteRef.current.value;
+    const deadline = deadlineRef.current.value;
+    noteRef.current.value = null;
+    deadlineRef.current.value = null;
+
+    const action = {
+      id: id,
+      label: "Reminder",
+      emailList: emailList,
+      reminder: reminder,
+      deadline: deadline,
+    };
+
+    setActions([...actions, action]);
+    setReminderOverlay(false);
+  };
+
+  const addMail = (event) => {
+    event.preventDefault();
+    setEmails([...emails, emailRef.current.value]);
+    emailRef.current.value = null;
+  };
 
   return (
-    <div>
+    <form onSubmit={addSetReminderAction}>
       <h1>Reminder</h1>
       <div className="h-24 w-full">
         <div className="flex h-8 items-center border-2 pl-2 rounded-md">
-          <HiClock className="inline" />
+          <HiClock className="inline text-lg" />
           <input
             type="email"
             placeholder="Enter email to add for a reminder"
             className="outline-none w-full pl-4"
+            ref={emailRef}
           />
-          <button className="w-2/12 rounded-md bg-blue-500 text-white h-full">
+          <button
+            className="w-2/12 rounded-md bg-blue-500 text-white h-full"
+            onClick={addMail}
+          >
             Add
           </button>
         </div>
+        {emails ? emails.map((email) => <span>{email}</span>) : null}
       </div>
       <h1>Reminder Note</h1>
       <textarea
         placeholder="Enter reminder note"
         className="w-full outline-none border-2 resize-none h-24 px-2 py-1"
-      ></textarea>
+        ref={noteRef}
+      />
       <div>
         <label className="block">Deadline</label>
-        <input type="datetime-local" />
+        <input type="datetime-local" ref={deadlineRef} />
       </div>
       <div className="mt-9">
         <button
-          onClick={addSetReminderAction}
+          type="submit"
           className="w-24 h-10 rounded-lg bg-green-600 text-white font-bold"
         >
           SUBMIT
@@ -51,7 +89,7 @@ const SetReminder = () => {
           CANCEL
         </button>
       </div>
-    </div>
+    </form>
   );
 };
 
